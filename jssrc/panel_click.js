71505eba6999ad80310019a558309b6e9f0954ac
Jacob Bruggemann <jbrugg@scripps.edu>
2019-06-24 15:44:58 -0500

    adding javascript

---
+++ jssrc/panel_click.js
@@ -0,0 +1,19 @@
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+	if(!$this.hasClass('panel-collapsed')) {
+		$this.parents('.panel').find('.panel-body').slideUp();
+		$this.addClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+	} else {
+		$this.parents('.panel').find('.panel-body').slideDown();
+		$this.removeClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+	}
+});
+
+// $('#markerSearch').keyup(function() {
+//     console.log('hello');
+//     dataTable.fnFilter(this.value);
+// });
+
+
